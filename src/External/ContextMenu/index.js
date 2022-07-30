4a05d21fa0bc2edc414d4230e0d94d030c6f0d88
Sam Groves <18527147+STGroves@users.noreply.github.com>
2022-07-31 00:18:12 +0100

    Code Update and Index update

--- src/External/ContextMenu/index.js
+++ src/External/ContextMenu/index.js
@@ -1,7 +1,7 @@
 import ContextMenu from './JS/ContextMenu';
 import CMButton from './JS/Elements/CMButton';
 import CMLabel from './JS/Elements/CMLabel';
-import CMSeparator from './JS/Elements/CMSeperator';
+import CMSeparator from './JS/Elements/CMSeparator';
 import CMSubMenu from './JS/Elements/CMSubMenu';
 
 export { ContextMenu, CMButton, CMLabel, CMSeparator, CMSubMenu };

dd341807936b369b3d356536048cf3724fc5edde
Sam Groves <18527147+STGroves@users.noreply.github.com>
2022-08-22 01:21:51 +0100

    Added Menu icons Updated PathCreator

--- src/External/PathCreator/PathCreator.js
+++ src/External/PathCreator/PathCreator.js
@@ -17,7 +17,7 @@ export default class PathCreator {
     if (!(position instanceof Vector) || !(direction instanceof Vector))
       throw new Error('Constructor parameters must be of type Vector');
 
-    if (!Vector.equals(position, Vector.Zero)) this.offset(position);
+    if (!position.equals(Vector.Zero)) this.offset(position);
     this.setDirection(direction);
   }
 
